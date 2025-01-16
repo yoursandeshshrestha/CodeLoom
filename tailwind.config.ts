dbafa9b1ead3ba9da6008890508a14bc440007ca
Sandesh Shrestha <yoursandeshgeneral@gmail.com>
2025-01-16 22:57:07 +0530

    Feat: Colors

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -9,8 +9,14 @@ export default {
   theme: {
     extend: {
       colors: {
-        background: "var(--background)",
-        foreground: "var(--foreground)",
+        background: "#090909", // Dark background
+        foreground: "#ffffff", // White text
+        primary: "#7c3aed", // Purple
+        secondary: "#a855f7", // Purple shade for gradients
+        accent: "#22d3ee", // Cyan
+        danger: "#ef4444", // Red
+        warning: "#facc15", // Yellow
+        success: "#4ade80", // Green
       },
     },
   },
